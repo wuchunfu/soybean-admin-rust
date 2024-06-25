c55fc3ed0d71584ccd82c395964ab75583600563
bytebytebrew <1571339199@qq.com>
2024-06-25 14:59:42 +0800

    🔊 feat: add web RequestId

--- server/initialize/src/router_initialization.rs
+++ server/initialize/src/router_initialization.rs
@@ -1,12 +1,29 @@
 use std::sync::Arc;
 
-use axum::{Extension, Router};
+use axum::{body::Body, Extension, Router};
+use server_middleware::{Request, RequestId, RequestIdLayer};
 use server_router::admin::{SysAuthenticationRouter, SysUserRouter};
 use server_service::admin::SysUserService;
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
 
 pub async fn initialize_admin_router() -> Router {
     let app = Router::new();
     app.merge(SysAuthenticationRouter::init_authentication_router().await)
         .merge(SysUserRouter::init_user_router().await)
         .layer(Extension(Arc::new(SysUserService)))
+        .layer(TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+            let request_id = request
+                .extensions()
+                .get::<RequestId>()
+                .map(ToString::to_string)
+                .unwrap_or_else(|| "unknown".into());
+            info_span!(
+                "request",
+                id = %request_id,
+                method = %request.method(),
+                uri = %request.uri(),
+            )
+        }))
+        .layer(RequestIdLayer)
 }
--- server/middleware/src/lib.rs
+++ server/middleware/src/lib.rs
@@ -1,1 +1,6 @@
 mod jwt;
+mod request_id;
+
+pub use http::Request;
+pub use jwt::jwt_auth_middleware;
+pub use request_id::{RequestId, RequestIdLayer};
