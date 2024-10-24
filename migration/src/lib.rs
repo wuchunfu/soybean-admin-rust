d2aa080dc05387ed0193075aee5e43b6e929eba7
bytebytebrew <wang1571339199@outlook.com>
2024-10-24 14:07:06 +0800

    🗃️ feat: add basic data migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,5 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 
+mod datas;
 mod schemas;
 pub struct Migrator;
 
@@ -7,6 +8,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
+            // 架构迁移
             Box::new(schemas::m20240815_082808_create_enum_status::Migration),
             Box::new(schemas::m20240815_082854_create_sys_user::Migration),
             Box::new(schemas::m20241023_091143_create_sys_menu::Migration),
@@ -20,6 +22,13 @@ impl MigratorTrait for Migrator {
             Box::new(schemas::m20241023_091204_create_sys_tokens::Migration),
             Box::new(schemas::m20241023_091210_create_sys_user_role::Migration),
             Box::new(schemas::m20241023_091159_create_sys_role_menu::Migration),
+            // 数据迁移
+            Box::new(datas::m20241023_102950_insert_sys_domain::Migration),
+            Box::new(datas::m20241024_033005_insert_sys_user::Migration),
+            Box::new(datas::m20241024_034526_insert_sys_role::Migration),
+            Box::new(datas::m20241024_034744_insert_sys_menu::Migration),
+            Box::new(datas::m20241024_033933_insert_sys_user_role::Migration),
+            Box::new(datas::m20241024_034305_insert_sys_role_menu::Migration),
         ]
     }
 }
