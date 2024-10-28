22a6a519b237c218e177a94bb004dcac24f7cea2
bytebytebrew <wang1571339199@outlook.com>
2024-10-28 18:20:18 +0800

    🚩 feat: add ip2region initialization

--- server/bin/src/main.rs
+++ server/bin/src/main.rs
@@ -12,6 +12,7 @@ async fn main() {
 
     server_initialize::initialize_log_tracing().await;
     server_initialize::initialize_config(config_path).await;
+    let _ = server_initialize::init_xdb().await;
     server_initialize::init_primary_connection().await;
     server_initialize::initialize_keys_and_validation().await;
     server_initialize::initialize_event_channel().await;
