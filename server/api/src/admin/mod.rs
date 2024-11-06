58b584c8eccd034ea040bf293e60b4be4336bd71
bytebytebrew <wang1571339199@outlook.com>
2024-11-06 16:51:32 +0800

    🚚 style(rename): unified naming convention for API directories

--- server/api/src/admin/mod.rs
+++ server/api/src/admin/mod.rs
@@ -1,17 +1,19 @@
-pub use sys_access_key::SysAccessKeyApi;
-pub use sys_authentication::SysAuthenticationApi;
+pub use sys_access_key_api::SysAccessKeyApi;
+pub use sys_authentication_api::SysAuthenticationApi;
 pub use sys_domain_api::SysDomainApi;
 pub use sys_endpoint_api::SysEndpointApi;
-pub use sys_login_log::SysLoginLogApi;
+pub use sys_login_log_api::SysLoginLogApi;
 pub use sys_menu_api::SysMenuApi;
+pub use sys_operation_log_api::SysOperationLogApi;
 pub use sys_role_api::SysRoleApi;
 pub use sys_user_api::SysUserApi;
 
-mod sys_access_key;
-mod sys_authentication;
+mod sys_access_key_api;
+mod sys_authentication_api;
 mod sys_domain_api;
 mod sys_endpoint_api;
-mod sys_login_log;
+mod sys_login_log_api;
 mod sys_menu_api;
+mod sys_operation_log_api;
 mod sys_role_api;
 mod sys_user_api;
